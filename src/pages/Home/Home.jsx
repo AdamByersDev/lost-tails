a71273ceb7035dac0e87907e24889453e27dc570
Adam Byers <adam@abyers.ca>
2025-02-27 17:06:07 -0500

    Removed text from top of home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,7 +5,6 @@ import AdoptionSection from '@/Components/AdoptionSection';
 export default function Home() {
   return (
     <main>
-      <h1>Lost Tails Home Page</h1>
       <Hero />
       <DonationSection />
       <AdoptionSection />
