075eb30796bf51d2446ac91e7573c4a702bbe446
Max Carmona <max870123@hotmail.com>
2025-02-27 00:11:22 -0500

    Fixing some tags and classes

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -4,24 +4,22 @@ import './Hero.css';
 
 export default function Hero() {
   return (
-    <section>
-      <div className="hero-header">
-        <article className="hero">
-          <h1>Helping Tails Find Their Way Home.</h1>
-          <div className="hero-buttons">
-            <button className="btn">Lost Pet</button>
-            <button className="btn">Found Pet</button>
-          </div>
-        </article>
-        <article>
-          <DotLottieReact
-            src="https://lottie.host/4821f0e5-2898-424d-977f-1fdf9b096b95/FBIbcKlPTr.lottie"
-            loop
-            autoplay
-            style={{ width: '100%', height: 'auto' }}
-          />
-        </article>
-      </div>
+    <section className="hero-header">
+      <article className="hero">
+        <h1>Helping Tails Find Their Way Home.</h1>
+        <div className="hero-buttons">
+          <button className="btn">Lost Pet</button>
+          <button className="btn">Found Pet</button>
+        </div>
+      </article>
+      <article>
+        <DotLottieReact
+          src="https://lottie.host/4821f0e5-2898-424d-977f-1fdf9b096b95/FBIbcKlPTr.lottie"
+          loop
+          autoplay
+          style={{ width: '100%', height: 'auto' }}
+        />
+      </article>
     </section>
   );
 }
