6d52205fe8efd52a666eaa45d32e6e59c01d596f
Nattar <aonkaseam.n@gmail.com>
2025-02-27 16:39:28 -0500

    first commit

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,12 @@
+import DonationSection from '@/Components/DonationSection';
+
 export default function Home() {
   return (
-    <section>
-      <h1>Lost Tails Home Page</h1>
-    </section>
+    <>
+      <section>
+        <h1>Lost Tails Home Page</h1>
+      </section>
+      <DonationSection />
+    </>
   );
 }
