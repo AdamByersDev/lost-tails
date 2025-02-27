546cf8e227c64d83e947e47af3b933b653a4e928
xlezzerx <151861662+xlezzerx@users.noreply.github.com>
2025-02-27 13:09:42 -0500

    Added donation section for landing page.  Used mobile first approach for styling.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Hero from '@/Components/Hero';
+import DonationSection from '@/Components/DonationSection';
 
 export default function Home() {
   return (
     <main>
       <h1>Lost Tails Home Page</h1>
       <Hero />
+      <DonationSection />
     </main>
   );
 }
