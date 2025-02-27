d9facff0edb84f1e2d365b9ab244ff0115449175
Adam Byers <adam@abyers.ca>
2025-02-27 13:28:16 -0500

    Fixed formatting issues

--- src/Components/AdoptionCard/AdoptionCard.jsx
+++ src/Components/AdoptionCard/AdoptionCard.jsx
@@ -1,17 +1,24 @@
 import './AdoptionCard.css';
+import PropTypes from 'prop-types';
 
 export default function AdoptionCard({ animal }) {
-  return (
-    <div className='adoptionCard'>
-      <img src={animal.primary_photo_cropped.small} />
-      <div>
-        <h4>{animal.name}</h4>
-        <p>{animal.contact.address.city}, {animal.contact.address.state}</p>
-        <p>
-          {animal.description}
-        </p>
-        <a href={animal.url}>More Information</a>
+  if (animal) {
+    return (
+      <div className="adoptionCard">
+        <img src={animal.primary_photo_cropped.small} />
+        <div>
+          <h4>{animal.name}</h4>
+          <p>
+            {animal.contact.address.city}, {animal.contact.address.state}
+          </p>
+          <p>{animal.description}</p>
+          <a href={animal.url}>More Information</a>
+        </div>
       </div>
-    </div>
-  )
+    );
+  }
 }
+
+AdoptionCard.propTypes = {
+  animal: PropTypes.object.isRequired,
+};
