e569f00dc20e08b55a3ae829972deeb95c9f3389
Nattar <aonkaseam.n@gmail.com>
2025-03-18 14:08:49 -0400

    change the prop

--- src/Components/DetailsPetSection/DetailsPetSection.jsx
+++ src/Components/DetailsPetSection/DetailsPetSection.jsx
@@ -1,71 +1,51 @@
 import './DetailsPetSection.css';
 import { Link } from 'react-router';
 
-const DetailsPetSection = () => {
-  return (
-    <>
-      <Link to={'lost-found'}>
-        <a className="go-back-btn">&larr; To go back</a>
-      </Link>
-      <div className="details-pet-container">
-        <section className="details-pet-section">
+export default function DetailsPetSection({ animal }) {
+  if (animal) {
+    return (
+      <>
+        <Link to={'lost-found'}>
+          <a className="go-back-btn">&larr; To go back</a>
+        </Link>
+        <div className="details-pet-container">
+          <div className="details-pet-status">{animal.status}</div>
           <img
-            src="src/assets/images/pic-lost-dog-1.jpg"
+            src={animal.primary_photo_cropped.small}
             alt="Lost Dog"
-            className="details-pet-section-img"
+            className="details-pet-img"
           />
-          <div className="profile-pet-card">
-            <div className="profile-pet-header">
-              <div className="profile-pet-name">
-                <span className="detail-pet-name">Danny</span>
-              </div>
+          <div className="data-pet-type">Description:</div>
+          <div className="detail-pet-description">{animal.description}</div>
+  
+          <section className="details-pet-section">
+            <div className="profile-pet-card">
+              <div className="data-pet-type">Date Reported:</div>
+              <div className="detail-pet-date">March 13, 2025</div>
+              <div className="data-pet-type">Last Location:</div>
+              <div className="detail-pet-location">{animal.address.city}</div>
+              <div className="data-pet-type">Name:</div>
+              <div className="detail-pet-breed">{animal.name}</div>
+              <div className="data-pet-type">Specie:</div>
+              <div className="detail-pet-specie">{animal.species}</div>
+              <div className="data-pet-type">Color:</div>
+              <div className="detail-pet-color">{animal.colors}</div>
+              <div className="data-pet-type">Size:</div>
+              <div className="detail-pet-size">{animal.size}</div>
+              <div className="data-pet-type">Gender:</div>
+              <div className="detail-pet-gender">{animal.gender}</div>
+              <div className="data-pet-type">Breed:</div>
+              <div className="detail-pet-breed">{animal.breeds}</div>
             </div>
-            <div className="profile-pet-info">
-              <div className="details-pet-info">
-                <span className="icon">📍</span>
-                <span>London, ON</span>
-              </div>
-              <div className="details-pet-info">
-                <span className="icon">🏠</span>
-                <span>West London, ON</span>
-              </div>
-              <div className="details-pet-info">
-                <span className="icon">⏳ lost</span>
-                <span>01/22/2024</span>
-              </div>
-              <div className="details-pet-info">
-                <span className="icon">👁️</span>
-                <span>18 views</span>
-              </div>
+            <div className="details-pet-actions">
+              <button className="details-pet-btn details-pet-btn-outline">
+                Contact the owner
+                <p>{animal.contact.email}</p>
+              </button>
             </div>
-          </div>
-          <div className="details-pet-about">
-            <h2>About the pet</h2>
-            <p>Danny was lost after being run over</p>
-            <p>
-              he had a small injury to his back paw, he loves to sleep in the
-              box.
-            </p>
-            <ul>
-              <li>Age: 2 years</li>
-              <li>Weight: 4 kg</li>
-              <li>Size: Small</li>
-              <li>Neutered, vaccinated, and good with other pets</li>
-            </ul>
-          </div>
-          <div className="details-pet-actions">
-            <button className="details-pet-btn details-pet-btn-outline">
-              Contact the owner
-            </button>
-            <button className="details-pet-btn details-pet-btn-primary">
-              I found this pet
-            </button>
-            <button className="details-pet-btn details-pet-btn-icon">🔗</button>
-          </div>
-        </section>
-      </div>
-    </>
-  );
+          </section>
+        </div>
+      </>
+    );
+  }
 };
-
-export default DetailsPetSection;
