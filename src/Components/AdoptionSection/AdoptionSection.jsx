55830773bfed49e648697acdb1a9239e4730651a
Adam Byers <adam@abyers.ca>
2025-03-18 15:47:19 -0400

    Added proper api call to home page adoption section

--- src/Components/AdoptionSection/AdoptionSection.jsx
+++ src/Components/AdoptionSection/AdoptionSection.jsx
@@ -1,4 +1,4 @@
-/* import { Client } from "@petfinder/petfinder-js"; */
+import { Client } from '@petfinder/petfinder-js';
 import { useEffect, useState } from 'react';
 import styles from './AdoptionSection.module.css';
 import mockData from './mockData.json';
@@ -7,38 +7,77 @@ import Button from '@/UI/Button';
 import { useNavigate } from 'react-router';
 import AdoptionPet from '../AdoptionPet';
 
-/* const client = new Client({
-  apiKey: "AV1FVzdrEyl86i7G3SiXZXEIIqSYjYhbOMFNEqi2xK3D7SGfkM",
-  secret: "d2QuDuCW7NlQLYC7cIr26hUkK8kvzU2O1fb2zMox"
-}); */
+const client = new Client({
+  apiKey: 'AV1FVzdrEyl86i7G3SiXZXEIIqSYjYhbOMFNEqi2xK3D7SGfkM',
+  secret: 'd2QuDuCW7NlQLYC7cIr26hUkK8kvzU2O1fb2zMox',
+});
 
 export default function AdoptionSection() {
-  const [adoptablePets /* , setAdoptablePets */] = useState(mockData);
+  const [adoptablePets, setAdoptablePets] = useState(mockData);
   const [adoptionCards, setAdoptionCards] = useState([]);
   const navigate = useNavigate();
 
   // redirect page when click button on adoption home section
   const handleRedirect = () => navigate('/adoption');
 
-  /* Pull the data from the API */
-  /* client.animal.search({
-    location: 'London, ON',
-    limit: 3,
-    distance: 25,
-  }).then((response) => {
-    let tempAdoptablePets = [];
-    response.data.animals.forEach(animal => {
-      tempAdoptablePets.push(animal)
-    });
-    console.log(tempAdoptablePets)
-    setAdoptablePets(tempAdoptablePets);
-  }); */
+  const getLocation = async () => {
+    try {
+      const response = await fetch('https://ipapi.co/json/');
+      if (!response.ok) {
+        throw new Error('Response status: ${response.status}');
+      }
+
+      const json = await response.json();
+      return `${json.city}, ${json.region_code}`;
+    } catch (e) {
+      console.error(e);
+      return 'London, ON';
+    }
+  };
+
+  useEffect(() => {
+    const getData = async () => {
+      /* Pull the data from the API */
+      let location = await getLocation();
+      console.log(location);
+      client.animal
+        .search({
+          location: location,
+          limit: 3,
+          distance: 50,
+        })
+        .then((response) => {
+          let tempAdoptablePets = [];
+          response.data.animals.forEach((animal) => {
+            console.log(animal);
+            let colors = animal.colors || { primary: 'Not set' };
+            let photo = animal.primary_photo_cropped || { small: null };
+            tempAdoptablePets.push({
+              id: animal.id,
+              name: animal.name,
+              description: animal.description,
+              specie: animal.species,
+              color: colors.primary,
+              size: animal.size,
+              gender: animal.gender,
+              breed: animal.breeds.primary,
+              picture: photo.small || 'None',
+              status: animal.status,
+              url: animal.url,
+            });
+          });
+          setAdoptablePets(tempAdoptablePets);
+        });
+    };
+    getData();
+  }, []);
+
   useEffect(() => {
     let tempAdoptionCards = [];
     adoptablePets.forEach((animal) => {
       tempAdoptionCards.push(
         <AdoptionPet
-          picture={animal.primary_photo_cropped.small}
+          picture={animal.picture}
           key={animal.id}
           name={animal.name}
           url={animal.url}
