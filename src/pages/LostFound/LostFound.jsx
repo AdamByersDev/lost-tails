4f5511157432dd4d039016d6df1442dfaf8c776d
Nattar <aonkaseam.n@gmail.com>
2025-03-20 11:40:48 -0400

    fixed router

--- src/pages/LostFound/LostFound.jsx
+++ src/pages/LostFound/LostFound.jsx
@@ -1,9 +1,9 @@
-import PetList from '@/Components/PetsList';
-import PetDetailsSection from '@/Components/PetDetailsSection';
-import { useParams } from 'react-router';
+import { Outlet } from 'react-router';
 
 export default function LostFound() {
-  const { id } = useParams();
-
-  return <main>{!id ? <PetList /> : <PetDetailsSection />}</main>;
+  return (
+    <main>
+      <Outlet />
+    </main>
+  );
 }
