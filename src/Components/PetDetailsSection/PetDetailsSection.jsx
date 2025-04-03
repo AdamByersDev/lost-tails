89d20fcae56d792a5f37ba8cb34e86365db74fe1
Adam Byers <adam@abyers.ca>
2025-04-02 21:01:40 -0400

    Added flyer creation

--- src/Components/PetDetailsSection/PetDetailsSection.jsx
+++ src/Components/PetDetailsSection/PetDetailsSection.jsx
@@ -9,13 +9,26 @@ import { useState } from 'react';
 import { Input, Modal } from 'antd';
 import { MailOutlined } from '@ant-design/icons';
 import Button from '@/UI/Button';
+import { useParams } from 'react-router';
+import QRCode from 'qrcode';
+import logo from '@/assets/images/icon.png';
+import {
+  Document,
+  Page,
+  pdf,
+  StyleSheet,
+  Text,
+  Image,
+  View,
+} from '@react-pdf/renderer';
 
 export default function PetDetailsSection() {
-  const { report } = useReport();
+  const { report, loading } = useReport();
   const [isMapOpen, setIsMapOpen] = useState(false);
   const [isContactOpen, setIsContactOpen] = useState(false);
   const [senderName, setSenderName] = useState('');
   const [customMessage, setCustomMessage] = useState('');
+  const { id } = useParams();
 
   if (!report) {
     return (
@@ -58,6 +71,94 @@ export default function PetDetailsSection() {
 
   const mailtoLink = `mailto:${email}?subject=${subject}&body=${body}`;
 
+  const printFlyer = async () => {
+    const qrCode = QRCode.toDataURL(
+      `https://adambyersdev.github.io/lost-tails/${id}`,
+    );
+
+    console.log(status);
+
+    const flyerStyles = StyleSheet.create({
+      page: {
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontFamily: 'Helvetica',
+        position: 'absolute',
+        padding: '1in',
+      },
+      title: {
+        fontSize: 64,
+        color: '#333',
+        fontWeight: 700,
+        paddingBottom: '.25in',
+      },
+      link: {
+        flexDirection: 'row',
+        height: '2in',
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      linkText: {
+        flex: 1,
+        textAlign: 'center',
+      },
+      linkURL: {
+        fontWeight: 700,
+      },
+      qr: {
+        flexShrink: 0,
+        aspectRatio: '1/1',
+      },
+      image: {
+        height: '5in',
+        width: '100%',
+        objectFit: 'contain',
+      },
+      watermarkWrapper: {
+        width: '100%',
+        flexDirection: 'row',
+        alignItems: 'flex-end',
+        justifyContent: 'flex-start',
+        fontSize: 12,
+        height: '0.25in',
+      },
+    });
+
+    const Flyer = () => (
+      <Document>
+        <Page size="LETTER" style={flyerStyles.page}>
+          <Text style={flyerStyles.title}>LOST PET</Text>
+          <Image src={picture} style={flyerStyles.image} />
+          <View style={flyerStyles.link}>
+            <View style={flyerStyles.linkText}>
+              <Text wrap={true}>
+                {status?.toLowerCase() == 'lost'
+                  ? `This is ${name || 'our pet'}, we cannot find ${gender?.toLowerCase() === 'male' ? 'him' : gender?.toLowerCase() === 'female' ? 'her' : 'them'} and we need your help. If you find ${name || 'this pet'}, please let me know by either scanning the QR code or by going to`
+                  : `I found this pet${name && `, ${name},`} and am hoping to find ${gender?.toLowerCase() === 'male' ? 'his' : gender?.toLowerCase() === 'female' ? 'her' : 'their'} owner. If you know ${name || 'this pet'}'s owner, please let me know by either scanning the QR code or by going to`}
+              </Text>
+              <Text style={flyerStyles.linkURL} wrap={false}>
+                github.com/AdamByersDev/lost-tails/
+              </Text>
+            </View>
+            <Image src={qrCode} style={flyerStyles.qr} />
+          </View>
+          <View style={flyerStyles.watermarkWrapper}>
+            <Image src={logo} />
+            <Text>Flyer generated with Lostails</Text>
+          </View>
+        </Page>
+      </Document>
+    );
+
+    const final = pdf(<Flyer />);
+
+    final.toBlob().then((blob) => {
+      window.open(URL.createObjectURL(blob), '_blank');
+    });
+  };
+
   return (
     <div className={styles.container}>
       <Link to="/lost-found" className={styles.goBackBtn}>
@@ -79,9 +180,11 @@ export default function PetDetailsSection() {
               />
             </div>
           )}
-          <Button className={styles.flyerBtn} onClick={() => {}}>
-            Print Flyer
-          </Button>
+          {!(loading || status?.toLowerCase() == 'complete') && (
+            <Button className={styles.flyerBtn} onClick={printFlyer}>
+              Print Flyer
+            </Button>
+          )}
         </div>
         <div className={styles.card}>
           <div className={styles.details}>
